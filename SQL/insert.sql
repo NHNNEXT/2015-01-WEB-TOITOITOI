9a2ce58c33deb1056851ff2a638785d023eb6b91
helloheesu <heesu@nhnnext.org>
2015-08-28 23:49:57 +0900

    sql: create table, insert data

---
+++ SQL/insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO `cafe` (`name`) VALUES ('test cafe1'), ('2nd cafe');
+INSERT INTO `post` (`cid`, `content`) VALUES (1, '포스트'), (1, '새 글'), (1, 'NEW'), (2, 'new'), (2, '하이하이');
+INSERT INTO `reply` (`pid`, `content`) VALUES (2, '댓글'), (2, '댓글2'), (4, '안녕');
---
+++ SQL/table.sql
@@ -0,0 +1,27 @@
+CREATE TABLE `cafe` (
+  `cid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  PRIMARY KEY (`cid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `post` (
+  `pid` int(11) NOT NULL AUTO_INCREMENT,
+  `content` varchar(20) NOT NULL,
+  `postingtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `cid` int(11) DEFAULT NULL,
+  `liked` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`pid`),
+  KEY `cid` (`cid`),
+  CONSTRAINT `post_ibfk_1` FOREIGN KEY (`cid`) REFERENCES `cafe` (`cid`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `reply` (
+  `reid` int(11) NOT NULL AUTO_INCREMENT,
+  `pid` int(11) NOT NULL,
+  `content` varchar(200) NOT NULL,
+  `liked` int(11) NOT NULL DEFAULT '0',
+  `postingtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`reid`),
+  KEY `pid` (`pid`),
+  CONSTRAINT `reply_ibfk_1` FOREIGN KEY (`pid`) REFERENCES `post` (`pid`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
