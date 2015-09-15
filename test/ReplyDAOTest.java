f031fe7faaf312d8b7a6e9c4221153d659e8bdf5
helloheesu <heesu@nhnnext.org>
2015-09-15 21:50:05 +0900

    correct junit test errors

--- test/ReplyDAOTest.java
+++ test/ReplyDAOTest.java
@@ -1,4 +1,4 @@
-package test;
+
 
 import static org.junit.Assert.assertNotNull;
 
@@ -12,24 +12,19 @@ import cafein.reply.Reply;
 import cafein.reply.ReplyDAO;
 
 public class ReplyDAOTest {
-	private Post TEST_POST;
 	private Reply TEST_REPLY;
 	private ReplyDAO replyDAO;
 
 	@Before
 	public void setup() {
-		TEST_POST = new Post(10, "testing post");
 		TEST_REPLY = new Reply("testing reply");
 		replyDAO = new ReplyDAO();
 	}
 
 	@Test
 	public void testAddReply() throws SQLException {
-		Post testingPost = TEST_POST;
 		Reply testingRe = TEST_REPLY;
-		assertNotNull(testingPost);
-		assertNotNull(testingRe);
-		replyDAO.addReply(testingRe, testingPost.getPid());
+		replyDAO.addReply(testingRe, 2);
 	}
 	//client에서 정상적으로 newlike갯수가 넘어왔을 때, sql에 liked +1 test
 	@Test
@@ -37,7 +32,6 @@ public class ReplyDAOTest {
 		Reply testingRe = TEST_REPLY;
 		testingRe.setReId(1);
 		assertNotNull(testingRe);
-		int newlike = 1;
-		replyDAO.likedOnReply(testingRe.getReId());
+		replyDAO.plusLike(testingRe.getReId());
 	}
  }
