46189618d86714556f05ee4aa7c11babe21476df
outofmap <enjoyhee@gmail.com>
2015-11-23 16:20:53 +0900

    changed folder path

--- src/main/java/cafein/cafe/GetNudgeController.java
+++ src/main/java/cafein/cafe/GetNudgeController.java
@@ -2,6 +2,7 @@ package cafein.cafe;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GetNudgeController {
 	
-	@RequestMapping(value="/nudge", method=RequestMethod.GET)
-	public List<Nudge> getNudge(@RequestParam Integer id) {
+	@RequestMapping(value="/nudge/{id}", method=RequestMethod.GET)
+	public List<Nudge> getNudge(@PathVariable Integer id) {
 		return (new NudgeDAO().getNudgeList(id));
 	}
 }
