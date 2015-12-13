11af31f4b936ea8a6edbbab195130a1a70c8aa61
helloheesu <heesu@nhnnext.org>
2015-12-13 17:09:46 +0900

    close #72

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,20 +1,29 @@
-
+var data = {};
+// var example = {
+// 	"뷰티토이" : [
+// 		{"postId":6,"dear":"뷰티토이","content":"슈테판이 간단한 사용법을 알려주었는데 , 일단은 SAP 언어셋팅을 영어로 바꾸는 것부터 해","createdtime":"2015-12-13 14:07:26.0","placeId":0,"likes":0},
+// 		{"postId":7,"dear":"뷰티토이","content":"해외에서 한국인 혹은 한국과 조금이라도 관련된 일을 하게 된다면 반갑지 않은 일을 만나게","createdtime":"2015-12-13 14:07:26.0","placeId":0,"likes":0}
+// 	], "깃토이" : [
+// 		{"postId":9,"dear":"깃토이","content":"잘 되나요?","createdtime":"2015-12-13 14:48:02.0","placeId":0,"likes":0},
+// 		{"postId":4,"dear":"깃토이","content":"깃 마스터 짱짱","createdtime":"2015-12-13 14:07:26.0","placeId":0,"likes":0},
+// 		{"postId":5,"dear":"깃토이","content":"프론트 대장님","createdtime":"2015-12-13 14:07:26.0","placeId":0,"likes":0}
+// 	], "toitoi" : [
+// 		{"postId":1,"dear":"toitoi","content":"깃토이, 뷰티토이, 플토이,소토이","createdtime":"2015-12-13 14:07:26.0","placeId":0,"likes":0},
+// 		{"postId":2,"dear":"toitoi","content":"윙가르디움 레비오우사","createdtime":"2015-12-13 14:07:26.0","placeId":0,"likes":0},
+// 		{"postId":3,"dear":"toitoi","content":"해리포터","createdtime":"2015-12-13 14:07:26.0","placeId":0,"likes":0}
+// 	]
+// };
 var matches = document.body.matchesSelector || document.body.webkitMatchesSelector || document.body.mozMatchesSelector || document.body.msMatchesSelector || document.body.webkitMatchesSelector || document.body.matchesSelector;
 
-data = [
-	{'dear':'손톱깎이', 'preview':['오리아 안녕 123456789', 'ㅈㄹㅈㅂㄷㄹㅂㅈ 안녕 123456789', 'ㅈㄷㄹㅈㅂㄷ 123456789ㅇ']},
-	{'dear':'오리', 'preview':['호호호ewfwef후후후 안녕 123456789', 'ipsum ㅈㄹㅈlorem ipuse 56789', 'lorem ㅈㄷipusum 6789ㅇ']},
-	{'dear':'오리2', 'preview':['호호호ewfwef후후후 안녕 123456789', 'ipsum ㅈㄹㅈlorem ipuse 56789', 'lorem ㅈㄷipusum 6789ㅇ']},
-	{'dear':'인하대후문', 'preview':['lrem imp789', 'ㅈipsrem islern 6789', 'ㅈㄷqwefqwefwqef wefsdaㅇ']},
-	{'dear':'오리3', 'preview':['호호호ewfwef후후후 안녕 123456789', 'ipsum ㅈㄹㅈlorem ipuse 56789', 'lorem ㅈㄷipusum 6789ㅇ']},
-	{'dear':'인하대후문2', 'preview':['lrem imp789', 'ㅈipsrem islern 6789', 'ㅈㄷqwefqwefwqef wefsdaㅇ']},
-	{'dear':'인하대후문2', 'preview':['lrem imp789', 'ㅈipsrem islern 6789', 'ㅈㄷqwefqwefwqef wefsdaㅇ']},
-	{'dear':'인하대후문2', 'preview':['lrem imp789', 'ㅈipsrem islern 6789', 'ㅈㄷqwefqwefwqef wefsdaㅇ']},
-	{'dear':'인하대후문2', 'preview':['lrem imp789', 'ㅈipsrem islern 6789', 'ㅈㄷqwefqwefwqef wefsdaㅇ']},
-	{'dear':'인하대후문2', 'preview':['lrem imp789', 'ㅈipsrem islern 6789', 'ㅈㄷqwefqwefwqef wefsdaㅇ']},
-	{'dear':'오리4', 'preview':['호호호ewfwef후후후 안녕 123456789', 'ipsum ㅈㄹㅈlorem ipuse 56789', 'lorem ㅈㄷipusum 6789ㅇ']},
-	{'dear':'인하대후문3', 'preview':['lrem imp789', 'ㅈipsrem islern 6789', 'ㅈㄷqwefqwefwqef wefsdaㅇ']}
-];
+function getKeys (data) {
+	var keys = [];
+	for (var key in data) {
+		if (data.hasOwnProperty(key)) {
+			keys.push(key);
+		}
+	}
+	return keys;
+}
 
 function renderDearList (dataList) {
 	var dataLen = dataList.length;
@@ -56,11 +65,16 @@ function addPreview (dataList, header) {
 }
 
 function findPreview (data, dear) {
-	var dataLen = data.length;
-	for (var i = 0; i < dataLen; i++) {
-		var current = data[i];
-		if (current.dear == dear) return current.preview;
+	var posts;
+	if (!data || !(posts = data[dear]) || !posts.length) {
+		debugger;
+		return null;
 	}
+	var result = [];
+	for (var i = 0; i < posts.length; i++) {
+		result.push(posts[i].content);
+	}
+	return result;
 }
 
 function cloneElement (el) {
@@ -71,14 +85,8 @@ function getDearListDone () {
 	var httpRequest = new XMLHttpRequest();
 	httpRequest.onreadystatechange = function(){
 	    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-	    	var data = JSON.parse(httpRequest.response);
-	    	var dears = [];
-	    	for (var key in data) {
-				if (data.hasOwnProperty(key)) {
-					dears.push(key);
-				}
-			}
-	    	renderDearList(dears);
+			data = JSON.parse(httpRequest.response);
+			renderDearList(getKeys(data));
 	    }
 	};
 	httpRequest.open('GET', '/api/place/1/dear?page=1', true);
@@ -89,20 +97,28 @@ document.addEventListener("DOMContentLoaded", function() {
 	getDearListDone();
 
 	document.querySelector('#letters').addEventListener('click', function (e) {
+		// nothing on -> anything on
+		// this on -> this goes off
+		// not this on -> on goes off, this goes on
+		// ---> previousOn(null, other, this)=off, currentOn(if different w/previousOn)=on
+
+		// if this has no data, append.
 		var header = e.target;
 		if (!header.matches('header')) {
 			return;
 		}
-		var list = header.parentNode.querySelector('ul');
+		var prevOn = this.querySelector('.on');
+		removeClass(prevOn, 'on');
+		console.log('compare', header, prevOn);
+		if (header === prevOn) { return; }
 
+		addClass(header, 'on');
+		var list = header.parentNode.querySelector('ul');
 		if (!list) {
-			var previews = findPreview(data, header.textContent);
+			var dear = header.textContent;
+			console.log('appending list', dear);
+			var previews = findPreview(data, dear);
 			addPreview(previews, header);
-			addClass(header, 'on');
-		} else {
-			var currentOn = this.querySelector('.on');
-			removeClass(currentOn, 'on');
-			if (header !== currentOn) {addClass(header, 'on');}
 		}
 	});
 });
