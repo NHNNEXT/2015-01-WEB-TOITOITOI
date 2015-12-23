3947b6935bc2d3fb3a0b536280ea552a3bcce439
helloheesu <heesu@nhnnext.org>
2015-12-23 21:11:30 +0900

    reflect change API : GET post list, needs refactoring

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,28 +1,77 @@
-function Dear (dearDataObject) {
+function Post (postDataObject) {
+	this.postId = postDataObject.id;
+	this.preview = postDataObject.content;
+	this.likes = postDataObject.likes;
+}
+
+// Post, Dear, PostList, DearList 로 나눠야겠다.
+
+function Dear (dearDataObject, placeId, listElementSelector) {
 	this.dearId = dearDataObject.dear_id;
 	this.name = dearDataObject.name;
 	this.maxPostNum = dearDataObject['COUNT(post.id)']; // change property name
 	this.currentPage = 0;
 	this.posts = [];
+	this.placeId = placeId;
+	this.listElementSelector = listElementSelector; // .querySelector occurs error for ''.
 }
-Dear.prototype.render = function () {
-	// body...
+Dear.prototype.render = function (listElement) {
+	var listElement = document.querySelector(this.listElementSelector);
+	if (!listElement) {
+		console.error('render to what?');
+		return;
+	}
+
+	var dataLen = this.posts.length;
+	var codes = '';
+	for (var i = 0; i < dataLen; i++) {
+		var currentPost = this.posts[i];
+		codes += '<li><a href="/place/'+this.placeId+'/dear/'+this.dearId+'/post/'+currentPost.postId+'">'+currentPost.preview+'</a></li>';
+	}
+	listElement.insertAdjacentHTML('beforeend', codes);
+};
+Dear.prototype.getNextPagePosts = function () {
+	if (this.currentPage > 0 && this.posts.length >= this.maxPostNum) {
+		console.log('all posts loaded');
+		return;
+	}
+
+	var httpRequest = new XMLHttpRequest(); ///api/place/1/dear/1/post?page=1
+	var requestURL = '/api/place/'+this.placeId+'/dear/'+this.dearId+'/post?page='+ ++(this.currentPage);
+	httpRequest.onreadystatechange = function () {
+	    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+			var received = JSON.parse(httpRequest.response);
+			if (!received.success) {
+				console.error('something went wrong @'+requestURL);
+				debugger;
+				return;
+			}
+			received.result.forEach(function (item, index, array) {
+				this.posts.push(new Post(item));
+			}.bind(this));
+			this.render();
+	    }
+	}.bind(this);
+	httpRequest.open('GET', requestURL, true);
+	httpRequest.send(null);
 };
 
-
-function DearList (placeId, maxDearNum) {
+function DearList (placeId, maxDearNum, listElement) {
 	this.placeId = placeId;
 	this.maxDearNum = maxDearNum;
 	this.currentPage = 0;
 	this.dears = []; // which is sorted by postNum DESC
+	this.listElement = listElement; // this file will be executed after parsing DOMs, becauseof 'defer'.
 }
 DearList.prototype.render = function () {
 	var dataLen = this.dears.length;
 	var codes = '';
 	for (var i = 0; i < dataLen; i++) {
-		codes += '<article><h3 data-index="'+i+'">'+this.dears[i].name+'</h3></article>';
+		// codes += '<article><h3 data-index="'+i+'">'+this.dears[i].name+'</h3><ul></ul></article>';
+		codes += '<article data-index="'+i+'"><h3>'+this.dears[i].name+'</h3><ul></ul></article>';
+		// codes += '<article><h3>'+this.dears[i].name+'</h3><ul></ul></article>';
 	}
-	document.querySelector('#letters').insertAdjacentHTML('beforeend', codes);
+	this.listElement.insertAdjacentHTML('beforeend', codes);
 };
 DearList.prototype.getNextPageDears = function () {
 	if (this.currentPage > 0 && this.dears.length >= this.maxDearNum) {
@@ -41,7 +90,7 @@ DearList.prototype.getNextPageDears = function () {
 				return;
 			}
 			received.result.forEach(function (item, index, array) {
-				this.dears.push(new Dear(item));
+				this.dears.push(new Dear(item, this.placeId, 'article[data-index="'+index+'"] ul'));
 			}.bind(this));
 			this.render();
 	    }
@@ -50,62 +99,9 @@ DearList.prototype.getNextPageDears = function () {
 	httpRequest.send(null);
 };
 
-function addPreview (dataList, header) {
-	if (!dataList || !header || !dataList.length) {
-		return;
-	}
-
-	var dataLen = dataList.length;
-	var codes = '<ul>';
-	for (var i = 0; i < dataLen; i++) {
-		var data = dataList[i];
-		codes += '<li><a href="'+window.location.pathname+'/dear/'+data.dear+'/post/'+data.postId+'">'+data.preview+'</a></li>';
-	}
-	codes += '</ul>';
-	header.insertAdjacentHTML('afterend', codes);
-}
-
-function findPreview (data, dear) {
-	var posts;
-	if (!data || !(posts = data[dear]) || !posts.length) {
-		return null;
-	}
-	var result = [];
-	for (var i = 0; i < posts.length; i++) {
-		var post = posts[i];
-		result.push({'dear':post.dear, 'preview':post.content, 'postId':post.postId});
-	}
-	return result;
-}
-
-function cloneElement (el) {
-	return JSON.parse(JSON.stringify(el));
-}
-
-// function getDearListDone () {
-// 	var httpRequest = new XMLHttpRequest();
-// 	var requestURL = '/api/place/1/dear?page=1';
-// 	httpRequest.onreadystatechange = function(){
-// 	    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-// 			var received = JSON.parse(httpRequest.response);
-// 			if (!received.success) {
-// 				console.error('something went wrong @'+requestURL);
-// 				debugger;
-// 				return;
-// 			}
-// 			data.dears = received.result;
-// 			renderDearList(data.dears.map(function (el, index, array) {
-// 				return el.name;
-// 			}));
-// 	    }
-// 	};
-// 	httpRequest.open('GET', requestURL, true);
-// 	httpRequest.send(null);
-// }
-
-var dearList = new DearList (1, 10);
 
 document.addEventListener("DOMContentLoaded", function() {
+	var dearList = new DearList (1, 10, document.querySelector('#letters'));
 	dearList.getNextPageDears();
 
 	document.querySelector('#new-letter form').addEventListener('submit', function (e) {
@@ -132,33 +128,28 @@ document.addEventListener("DOMContentLoaded", function() {
 	});
 
 	document.querySelector('#letters').addEventListener('click', function (e) {
-		var header = e.target;
-		if (!header.matches('h3')) {
-			if (header.matches('article')) {
-				header = header.querySelector('h3');
-				console.log(header);
-			} else {
-				return;
-			}
+		if (!e.target.matches('h3') && !e.target.matches('article')) { // check delegation target
+			return;
 		}
+		var parentElement = e.target.closest('article');
+		var header = parentElement.querySelector('h3');
+		var list = parentElement.querySelector('ul');
+
 		var prevOn = this.querySelector('.on');
 		removeClass(prevOn, 'on');
 		if (header === prevOn) { return; }
 
 		addClass(header, 'on');
-		var list = header.parentNode.querySelector('ul');
-		if (!list) {
-			var dear = header.textContent;
-			getDearListDone();
-			var previews = findPreview(data, dear);
-			addPreview(previews, header);
-		}
+		var elementIndex = parentElement.getAttribute('data-index'); // .dataset doesn't work for IE10.
+
+		var dear = dearList.dears[elementIndex];
+		dear.getNextPagePosts();
+		// debugger;
+		// var list = header.parentNode.querySelector('ul');
+		// if (!list) {
+		// 	var dear = header.textContent;
+		// 	var previews = findPreview();
+		// 	addPreview(previews, header);
+		// }
 	});
 });
-
-
-function updateReplies(reply){
-	console.log(reply);
-	var code = '<article class="reply">re : '+reply.content+'</article>'
-	document.querySelector('#replies').insertAdjacentHTML('afterbegin', code);
-}
