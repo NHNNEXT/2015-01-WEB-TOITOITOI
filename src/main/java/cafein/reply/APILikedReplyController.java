2d348839aab42e9eb6c91d16c5d06b6e73d8825f
helloheesu <heesu@nhnnext.org>
2016-01-06 03:19:09 +0900

    like it like it i love it
    close #134, close #130

--- src/main/java/cafein/reply/APILikedReplyController.java
+++ src/main/java/cafein/reply/APILikedReplyController.java
@@ -3,12 +3,14 @@ package cafein.reply;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import cafein.util.IllegalAPIPathException;
 import cafein.util.Result;
+import cafein.util.Validation;
 
 @RestController
 public class APILikedReplyController {
@@ -17,10 +19,12 @@ public class APILikedReplyController {
 	@Autowired
 	private ReplyDAO replydao;
 
-	@RequestMapping(value = "/likedOnReply", method = RequestMethod.GET)
-	protected Result like(@RequestParam(value = "reid") int replyId, @RequestParam String status) {
+	@RequestMapping(value = "/api/reply/{replyid}/like", method = RequestMethod.GET)
+	protected Result like(@PathVariable int replyid) {
+		if (!Validation.isValidParameter(replyid) || !Validation.isValidParameterType(replyid)) {
+			throw new IllegalAPIPathException();
+		}
 
-		replydao.plusLike(replyId);
-		return Result.success();
+		return Result.success(replydao.plusLike(replyid));
 	}
 }
