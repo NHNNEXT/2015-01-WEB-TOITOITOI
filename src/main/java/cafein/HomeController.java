159b7dbce37897e93d3e6d206f17f450636070c8
helloheesu <heesu@nhnnext.org>
2016-01-26 14:38:19 +0900

    url changed

--- src/main/java/cafein/HomeController.java
+++ src/main/java/cafein/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String home(){
-		return "redirect:/place/1";
+		return "redirect:/3";
 	}
 	
 }
